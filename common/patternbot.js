2fb2bb50e32a5a391dd14ee8d05ee3149e114a90
Alex Spagnuolo <spag0022@algonquinlive.com>
2018-03-22 08:56:13 -0400

    Include the cards

--- common/patternbot.js
+++ common/patternbot.js
@@ -388,9 +388,9 @@ const patternBotIncludes = function (manifest) {
 /** 
  * Patternbot library manifest
  * /Users/Test/Library/Mobile Documents/com~apple~CloudDocs/Year 2/Semester 5/Web Development IV/Week 10 - Home page template/geohub
- * @version 1521723327476
+ * @version 1521723366137
  */
-const patternManifest_1521723327476 = {
+const patternManifest_1521723366137 = {
   "commonInfo": {
     "modulifier": [
       "responsive",
@@ -846,5 +846,5 @@ const patternManifest_1521723327476 = {
   }
 };
 
-patternBotIncludes(patternManifest_1521723327476);
+patternBotIncludes(patternManifest_1521723366137);
 }());
